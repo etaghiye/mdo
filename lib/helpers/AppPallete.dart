fedc4df3be639b0e9c86a98314b39b0f64c8182c
Emil <emil.taghiyev@yahoo.com>
2024-06-19 20:30:54 +0400

    Registration

--- lib/helpers/AppPallete.dart
+++ lib/helpers/AppPallete.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppPallete {
   static const backgroundColor = Color.fromRGBO(91, 91, 104, 1.0);
+  static const gradient1 = Color.fromRGBO(187, 63, 221, 1.0);
 }
